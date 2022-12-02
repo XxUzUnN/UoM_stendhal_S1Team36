bf0bbd1908655fc8d639ae0d51d17d06eb4015d1
chaSenecHo <101160308+chaSenecHo@users.noreply.github.com>
2022-12-02 00:38:49 +0000

    Add basic code for pipe
    Create a new item pipe with description.
    Make it visible in the game and can be
    summoned.
    
    Change File: Pipe.java, pipe.png, tools.xml
    Issue: #11

--- src/games/stendhal/server/entity/item/Pipe.java
+++ src/games/stendhal/server/entity/item/Pipe.java
@@ -1,4 +1,7 @@
 package games.stendhal.server.entity.item;
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.creature.Creature;
+import games.stendhal.server.entity.player.Player;
 import java.util.Map;
 
 public class Pipe extends Item {
@@ -7,6 +10,45 @@ public class Pipe extends Item {
                 final Map<String, String> attributes) {
         super(name, clazz, subclass, attributes);
 
+        setPersistent(true);
+    }
+
+    public Pipe(final Pipe pipe) {
+        super(pipe);
+    }
+
+    @Override
+    public String describe() {
+        return "You see a pipe. It prevents the player from being attacked by creatures.";
+    }
+
+    @Override
+    public String getName() {
+        return "pipe";
+    }
+
+    //player doesn't have pipe
+    public boolean notCharmed(final RPEntity user) {
+        if (user instanceof Player) {
+            final Player player = (Player) user;
+            if (player.has("pipe")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //player has pipe
+    //make sure player can't be targeted by creatures
+    public boolean charmedByPipe(final RPEntity user) {
+        if (user instanceof Player) {
+            final Player player = (Player) user;
+            if (player.getEquippedItemClass("lhand", "pipe") != null) {
+                //uncompleted
+                return true;
+            }
+        }
+        return false;
     }
 
 }
