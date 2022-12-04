09190f6057d916004ff64a951c4c664edaad4fc4
Rui Bi <rui.bi@student.manchester.ac.uk>
2022-12-04 17:28:39 +0000

    Add Test for bank statements
    In this commit, I added a failing test for the bank statement feature to be implemented.
    
    Changed file: (new) BankStatementTest.java
    Issue: #12

--- tests/games/stendhal/client/gui/BankStatementTest.java
+++ tests/games/stendhal/client/gui/BankStatementTest.java
@@ -11,10 +11,10 @@ import games.stendhal.client.gui.progress.BankStatement;
  * tests that the list of bank names that will be displayed in the travel log are correct
  */
 public class BankStatementTest {
-	private List<String> testList = Arrays.asList("Semos", "Ados", "Deniran", "Fado", "Nalwor", "Zaras");
-	
-	@Test
-	public void testBankNamesArray() {
-		assertEquals(testList, BankStatement.bankStatementNames);
-	}
+ private List<String> testList = Arrays.asList("Semos", "Ados", "Deniran", "Fado", "Nalwor", "Zaras");
+ 
+ @Test
+ public void testBankNamesArray() {
+  assertEquals(testList, BankStatement.bankStatementNames);
+ }
 }
