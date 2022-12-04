7f9652ef8e8013b570b4d75f340522178501afdd
Anandajyothis Binu <anandajyothis.binu@student.manchester.ac.uk>
2022-12-04 20:07:59 +0000

    Add fixes to BankTellerNPCTest.java
    In this commit, I added bug fixes to BankTellerNPCTest.
    
    Changed file: BankTellerNPCTest.java
    Issue: #12

--- tests/games/stendhal/server/maps/ados/bank/BankTellerNPCTest.java
+++ tests/games/stendhal/server/maps/ados/bank/BankTellerNPCTest.java
@@ -13,60 +13,46 @@
 package games.stendhal.server.maps.ados.bank;
 
 import games.stendhal.server.core.engine.SingletonRepository;
-import games.stendhal.server.core.engine.StendhalRPZone;
-import games.stendhal.server.entity.npc.NPC;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.maps.ados.bank.BankNPC;
-
-import org.junit.Test;
-import utilities.PlayerTestHelper;
 import utilities.ZonePlayerAndNPCTestImpl;
-
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static utilities.SpeakerNPCTestHelper.getReply;
+import utilities.QuestHelper;
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.Before;
 
 
 public class BankTellerNPCTest extends ZonePlayerAndNPCTestImpl {
 
-    private static final String ZONE_NAME = "testzone";
+    private static final String ZONE_NAME = "int_ados_bank_";
 
     private static final String npcName = "Yance";
-
-    public BankTellerNPCTest() {
-        setNpcNames(npcName);
-        setZoneForPlayer(ZONE_NAME);
-        addZoneConfigurator(new BankNPC(), ZONE_NAME);
-    }
-
-
-    @Test
-    public void testPipeDealerNpc(){
-        final SpeakerNPC npc = getNPC(npcName);
-        final Player player = PlayerTestHelper.createPlayer("bob");
-        assertEquals("Yance is the Ados bank teller. He can help you get your bank statements.", npc.getDescription());
-
-        npc.getEngine().step(player, "statements");
-        assertEquals("this feature's functionality has not been implemented yet.", getReply(npc));
-        
-    }
     
-    @Test
-	public void testConfigureZone() {
-
-		SingletonRepository.getRPWorld();
-		final BankNPC bankTellerNPC = new BankNPC();
-
-		final StendhalRPZone zone = new StendhalRPZone("testzone");
-		bankTellerNPC.configureZone(zone, null);
-		assertFalse(zone.getNPCList().isEmpty());
-		final NPC bankTeller = zone.getNPCList().get(1);
-		assertThat(bankTeller.getName(), is("Yance"));
-		assertThat(bankTeller.getDescription(), is("Yance is the Ados bank teller. He can help you get your bank statements."));
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+		setupZone(ZONE_NAME);
 	}
+
+	
+    public BankTellerNPCTest() {
+    	super(ZONE_NAME, npcName);
+    }
     
-    
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		new BankNPC().configureZone(zone, null);
+	}
+
+
+    @Test
+    public void TellerNPCTest(){
+    	SpeakerNPC teller = SingletonRepository.getNPCList().get(npcName);
+        assertEquals("Yance is the Ados bank teller. He can help you get your bank statements.", teller.getDescription());
+        
+    }  
 }
 
 	
